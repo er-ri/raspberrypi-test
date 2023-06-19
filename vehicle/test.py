92546cf0576e2063c8176d1a8c84bf8b02d28dae
er-ri <724chen@gmail.com>
2023-06-19 15:35:47 +0900

    a

--- vehicle/test.py
+++ vehicle/test.py
@@ -10,3 +10,19 @@ print(hub)
 # hub.display.char('a')
 # hub.light.off()
 hub.light.on(Color.RED)
+
+# Raspberry Pi4 
+serial = hub.port.C
+serial.mode(hub.port.MODE_FULL_DUPLEX)
+serial.baud(115200)
+
+# Initialize serial port connected to LEGO SPIKE PRIME
+serialPort = serial.Serial(
+    port='/dev/ttyAMA1',
+    baudrate = 115200,
+    parity=serial.PARITY_NONE,
+    stopbits=serial.STOPBITS_ONE,
+    bytesize=serial.EIGHTBITS,
+    timeout=1
+)
+
