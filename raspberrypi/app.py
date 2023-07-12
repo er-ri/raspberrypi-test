c265c1884103fa48dfec90a8175801946c4fde8d
er-ri <724chen@gmail.com>
2023-07-12 14:49:59 +0900

    Update

---
+++ raspberrypi/app.py
@@ -0,0 +1,54 @@
+"""Responder between Raspberry PI and PC
+The module runs on native Raspberry PI environment.
+Using the following command to launch the flask server:
+    flask run --host=0.0.0.0
+"""
+
+import cv2 
+import serial 
+from flask import Flask, Response, render_template, request 
+
+cam = cv2.VideoCapture(0)
+
+serialPort = serial.Serial(
+    port='/dev/ttyAMA1',
+    baudrate = 115200,
+    parity=serial.PARITY_NONE,
+    stopbits=serial.STOPBITS_ONE,
+    bytesize=serial.EIGHTBITS,
+    timeout=1
+)
+
+def gen_frames():
+    while True:
+        success, frame = cam.read()
+        if not success:
+            break
+        else:
+            ret, buffer = cv2.imencode(".jpg", frame)
+            frame = buffer.tobytes()
+            yield (
+                b"--frame\r\n" b"Content-Type: image/jpeg\r\n\r\n" + frame + b"\r\n"
+            )
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/video_feed")
+def video_feed():
+    return Response(gen_frames(), mimetype="multipart/x-mixed-replace; boundary=frame")
+
+@app.route("/cmd", methods=["POST"])
+def cmd():
+    global serialPort
+    requestData = request.form["type"]
+    print("Sending command to SPIKE: {}".format(requestData))
+    if serialPort:
+        serialPort.write(requestData.encode())
+    return "Success!"
+ 
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ raspberrypi/controller.py
@@ -0,0 +1,31 @@
+"""Controller for SPIKE Prime
+The module runs on Raspberry PI MicroPython environment.
+Using the following command to run the file:
+    python pyboard.py --device /dev/ttyACM0 controller.py
+"""
+import json
+import hub,utime
+
+# Raspberry Pi4 
+serial = hub.port.D
+serial.mode(hub.port.MODE_FULL_DUPLEX)
+serial.baud(115200)
+
+# Motors
+left = hub.port.C.motor
+right = hub.port.D.motor
+
+left.pwm(0)
+right.pwm(0)
+
+while True : 
+    # Read communcition from Raspberry Pi and Decode
+    data = serial.read(1000)
+    data = data.decode("UTF-8")
+    parameters = json.loads(data)
+    
+    speed = parameters["speed"]
+    left.pwm(speed)
+    right.pwm(speed)
+
+    utime.sleep_ms(100)
